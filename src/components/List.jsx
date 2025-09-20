cdaeb799e7b7daa69be7d960c0caf37cea8f2d38
looks32 <wheodnjs2@naver.com>
2025-09-20 16:53:51 +0900

    useMemo 학습

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 import TodoItem from './TodoItem';
 
 const List = ({ todos, onUpdate, onDelete }) => {
@@ -15,17 +15,48 @@ const List = ({ todos, onUpdate, onDelete }) => {
     );
   };
 
+  // const getAnalyzedDate = () => {
+  //   const totalCount = todos.length;
+  //   const doneCount = todos.filter((todo) => todo.isDone).length;
+  //   const notDoneCount = totalCount - doneCount;
+  //   return {
+  //     totalCount,
+  //     doneCount,
+  //     notDoneCount,
+  //   };
+  // };
+
+  // const { totalCount, doneCount, notDoneCount } = getAnalyzedDate();
+
+  // 위의 주석 내용을
+  const { totalCount, doneCount, notDoneCount } = useMemo(() => {
+    // 이제는 검색을 해도 console이 찍히지 않음
+    console.log('호출');
+    const totalCount = todos.length;
+    const doneCount = todos.filter((todo) => todo.isDone).length;
+    const notDoneCount = totalCount - doneCount;
+    return {
+      totalCount,
+      doneCount,
+      notDoneCount,
+    };
+  }, [todos]);
+
   const filteredTodos = getFilteredData();
   return (
     <div>
       <h4>Todo List</h4>
+      <div>
+        <div>total : {totalCount}</div>
+        <div>done : {doneCount}</div>
+        <div>notDone : {notDoneCount}</div>
+      </div>
       <input
         type="text"
         onChange={onChangeSearch}
         value={search}
         placeholder="검색어를 입력하세요"
       />
-      <button>추가</button>
       {filteredTodos.map((todo) => {
         return (
           <TodoItem
